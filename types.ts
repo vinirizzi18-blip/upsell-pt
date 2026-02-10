03ade29359e3ccc53c91289c7223990a2876f59f
vinirizzi18-blip <vini.rizzi18@gmail.com>
2026-02-10 10:37:51 -0300

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,14 @@
+
+export interface Testimonial {
+  id: string;
+  name: string;
+  role: string;
+  content: string;
+  avatar: string;
+}
+
+export interface Benefit {
+  title: string;
+  description: string;
+  icon: string;
+}
